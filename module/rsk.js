7df3010d07b1d5d6a88208f370964674be8cf44d
Quinten Marsala <quinten.marsala@gmail.com>
2023-11-16 23:32:06 -0800

    fix actor registration

--- module/rsk.js
+++ module/rsk.js
@@ -5,9 +5,10 @@ Hooks.once("init", function () {
     console.log("initializing...");
 
     Items.unregisterSheet("core", ItemSheet)
-    Items.unregisterSheet("core", ActorSheet)
     Items.registerSheet("rsk", RSKItemSheet, { makeDefault: true })
-    Items.registerSheet("rsk", RSKActorSheet, { makeDefault: true })
+
+    Actors.unregisterSheet("core", ActorSheet)
+    Actors.registerSheet("rsk", RSKActorSheet, { makeDefault: true })
 
     console.log("rsk ready");
 });
