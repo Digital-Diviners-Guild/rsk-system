dc63105d815c59a4babd1e09eb2e8cb03b9d5c15
Quinten Marsala <quinten.marsala@gmail.com>
2023-11-16 20:47:19 -0800

    update to not use deprecrated data prop

--- module/sheets/RSKItemSheet.js
+++ module/sheets/RSKItemSheet.js
@@ -1,5 +1,5 @@
 export default class RSKItemSheet extends ItemSheet {
     get template() {
-        return `systems/rsk-system/templates/sheets/${this.item.data.type}-sheet.html`;
+        return `systems/rsk-system/templates/sheets/${this.item.type}-sheet.html`;
     }
 }
