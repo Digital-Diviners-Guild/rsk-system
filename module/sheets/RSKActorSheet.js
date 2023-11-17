6a86f4d76a79abedd6f823d0d1a674cf759d221f
Quinten Marsala <quinten.marsala@gmail.com>
2023-11-16 23:25:53 -0800

    implement getData in actor

--- module/sheets/RSKActorSheet.js
+++ module/sheets/RSKActorSheet.js
@@ -2,4 +2,8 @@ export default class RSKActorSheet extends ActorSheet {
     get template() {
         return `systems/rsk-system/templates/actors/${this.item.type}-sheet.html`;
     }
+
+    getData() {
+        return super.getData()
+    }
 }
