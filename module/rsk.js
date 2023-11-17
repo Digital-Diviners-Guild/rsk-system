c4212d6b3f419c64582a934e24e9d1fbd12b82a0
Quinten Marsala <quinten.marsala@gmail.com>
2023-11-16 23:15:47 -0800

    register actor sheet

--- module/rsk.js
+++ module/rsk.js
@@ -1,10 +1,13 @@
 import RSKItemSheet from "./sheets/RSKItemSheet.js";
+import RSKActorSheet from "./sheets/RSKActorSheet.js";
 
 Hooks.once("init", function () {
     console.log("initializing...");
 
     Items.unregisterSheet("core", ItemSheet)
+    Items.unregisterSheet("core", ActorSheet)
     Items.registerSheet("rsk", RSKItemSheet, { makeDefault: true })
+    Items.registerSheet("rsk", RSKActorSheet, { makeDefault: true })
 
     console.log("rsk ready");
 });
