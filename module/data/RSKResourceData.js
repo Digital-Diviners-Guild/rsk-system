56cc291be38ba7353412b671c1273dee944f7b76
Quinten Marsala <quinten.marsala@gmail.com>
2023-11-18 09:34:17 -0800

    use actually a posotive number for initial

--- module/data/RSKResourceData.js
+++ module/data/RSKResourceData.js
@@ -5,7 +5,7 @@ export default class RSKResourceData extends foundry.abstract.DataModel {
             name: new fields.StringField({ required: true, blank: false, initial: "new resource" }),
             uses: new fields.StringField({}),
             effects: new fields.StringField({}),
-            cost: new fields.NumberField({ required: true, nullable: false, integer: true, positive: true, initial: 0 }),
+            cost: new fields.NumberField({ required: true, nullable: false, integer: true, positive: true, initial: 1 }),
             tags: new fields.ArrayField(new fields.StringField()),
             gatheringRequirements: new fields.SchemaField({
                 equipmentRequired: new fields.SchemaField({
@@ -17,7 +17,7 @@ export default class RSKResourceData extends foundry.abstract.DataModel {
                 resourcesNeeded: new fields.ArrayField(
                     new fields.SchemaField({
                         name: new fields.StringField({}),
-                        quantity: new fields.NumberField({ nullable: false, integer: true, positive: true, initial: 0 })
+                        quantity: new fields.NumberField({ nullable: false, integer: true, positive: true, initial: 1 })
                     })),
                 equipmentRequired: new fields.SchemaField({
                     name: new fields.StringField({ initial: "none" }),
