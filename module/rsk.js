a428f83cafbc6a730f634c1dca1fd87b983dfa6e
Quinten Marsala <quinten.marsala@gmail.com>
2023-11-18 13:37:46 -0800

    back to template json

--- module/rsk.js
+++ module/rsk.js
@@ -1,6 +1,5 @@
 import RSKItemSheet from "./sheets/RSKItemSheet.js";
 import RSKActorSheet from "./sheets/RSKActorSheet.js";
-import RSKResourceData from "./data/RSKResourceData.js";
 
 async function preloadHandlebarsTemplates() {
     const templatePaths = [];
@@ -12,7 +11,6 @@ Hooks.once("init", function () {
 
     Items.unregisterSheet("core", ItemSheet)
     Items.registerSheet("rsk", RSKItemSheet, { makeDefault: true })
-    CONFIG.Item.dataModels.resource = RSKResourceData;
     
     Actors.unregisterSheet("core", ActorSheet)
     Actors.registerSheet("rsk", RSKActorSheet, { makeDefault: true })
