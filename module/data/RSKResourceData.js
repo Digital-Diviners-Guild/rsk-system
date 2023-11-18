419ff8d480a9be1a45ea08569a174efb059d3e4a
Quinten Marsala <quinten.marsala@gmail.com>
2023-11-18 09:29:34 -0800

    remove required most plaecs, add initial values

--- module/data/RSKResourceData.js
+++ module/data/RSKResourceData.js
@@ -2,25 +2,25 @@ export default class RSKResourceData extends foundry.abstract.DataModel {
     static defineSchema() {
         const fields = foundry.data.fields;
         return {
-            name: new fields.StringField({ required: true, blank: false }),
-            uses: new fields.StringField({ required: true, blank: false }),
-            effects: new fields.StringField({ required: true, blank: false }),
-            cost: new fields.NumberField({ required: true, nullable: false, integer: true, positive: true }),
+            name: new fields.StringField({ required: true, blank: false, initial: "new resource" }),
+            uses: new fields.StringField({}),
+            effects: new fields.StringField({}),
+            cost: new fields.NumberField({ required: true, nullable: false, integer: true, positive: true, initial: 0 }),
             tags: new fields.ArrayField(new fields.StringField()),
             gatheringRequirements: new fields.SchemaField({
                 equipmentRequired: new fields.SchemaField({
-                    name: new fields.StringField({ required: true, blank: false })
+                    name: new fields.StringField({ initial: "none" })
                 }),
-                location: new fields.SetField(new fields.NumberField({ nullable: false, min: 0, max: 1 }))
+                location: new fields.StringField({ initial: "any" })
             }),
             craftingRequirements: new fields.SchemaField({
                 resourcesNeeded: new fields.ArrayField(
                     new fields.SchemaField({
-                        name: new fields.StringField({ required: true, blank: false }),
-                        quantity: new fields.NumberField({ required: true, nullable: false, integer: true, positive: true })
+                        name: new fields.StringField({}),
+                        quantity: new fields.NumberField({ nullable: false, integer: true, positive: true, initial: 0 })
                     })),
                 equipmentRequired: new fields.SchemaField({
-                    name: new fields.StringField({ required: true, blank: false }),
+                    name: new fields.StringField({ initial: "none" }),
                 })
             })
         }
