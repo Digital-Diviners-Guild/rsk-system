1aacd839db909317c82eb44c90a46502c461c102
Quinten Marsala <quinten.marsala@gmail.com>
2023-11-18 17:10:29 -0800

    add document to game context

--- module/rsk.js
+++ module/rsk.js
@@ -1,6 +1,7 @@
+import RSKItem from "./documents/RSKItem.js";
+
 import RSKItemSheet from "./sheets/RSKItemSheet.js";
 import RSKActorSheet from "./sheets/RSKActorSheet.js";
-import RSKItem from "./documents/RSKItem.js";
 
 async function preloadHandlebarsTemplates() {
     const templatePaths = [];
@@ -9,10 +10,14 @@ async function preloadHandlebarsTemplates() {
 
 Hooks.once("init", function () {
     console.log("initializing...");
+    game.rsk = {
+        RSKItem
+    }
+
+    CONFIG.Item.documentClass = RSKItem;
 
     Items.unregisterSheet("core", ItemSheet)
     Items.registerSheet("rsk", RSKItemSheet, { makeDefault: true })
-    CONFIG.Item.documentClass = RSKItem;
 
     Actors.unregisterSheet("core", ActorSheet)
     Actors.registerSheet("rsk", RSKActorSheet, { makeDefault: true })
