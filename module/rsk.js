5c3cbb7d78533577415792ce7b8325968b4e50e0
Quinten Marsala <quinten.marsala@gmail.com>
2023-11-22 16:36:20 -0800

    refactor template

--- module/rsk.js
+++ module/rsk.js
@@ -13,7 +13,9 @@ globalThis.rsk = {
 async function preloadHandlebarsTemplates() {
     const templatePaths = [
         "/systems/rsk-system/templates/items/parts/edit-uses-effects.hbs",
-        "/systems/rsk-system/templates/items/parts/uses-effects.hbs"
+        "/systems/rsk-system/templates/items/parts/uses-effects.hbs",
+        "/systems/rsk-system/templates/actors/parts/view-actions.hbs",
+        "/systems/rsk-system/templates/actors/parts/edit-actions.hbs"
     ];
     return loadTemplates(templatePaths);
 }
@@ -24,7 +26,7 @@ Hooks.once("init", function () {
         RSKItem,
         RSKActor
     };
-
+    
     CONFIG.RSK = RSK;
     CONFIG.Actor.documentClass = RSKActor;
     CONFIG.Item.documentClass = RSKItem;
