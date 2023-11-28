ebfc6a9a8621a0c135683d57c0e8ca1cf68d4962
Quinten Marsala <quinten.marsala@gmail.com>
2023-11-27 18:12:04 -0800

    correct the dice chat colors for min max rolls

--- module/rsk-dice.js
+++ module/rsk-dice.js
@@ -1,13 +1,12 @@
 //todo:
 //check for triples
 //check for success (need to know the TN)
-//can we make 1 green and 6 red in the chat message?
 
 export default class RSKDice {
     static addButtonListener = (html) => {
         html.find('.roll-dice').click(async ev => {
             //for now a simple way to roll 3d6, later - cool stuff
-            let r = new Roll("3d6");
+            let r = Roll.create("3d6");
             await r.evaluate();
             r.toMessage({});
         });
