effefd639cddeecc335de5c9dc69828dbe59ccb6
Quinten Marsala <quinten.marsala@gmail.com>
2023-12-01 21:50:34 -0800

    refactor

--- module/documents/RSKActor.js
+++ module/documents/RSKActor.js
@@ -12,20 +12,8 @@ export default class RSKActor extends Actor {
   prepareBaseData() {
     // Data modifications in this step occur before processing embedded
     // documents or derived data.
-
-    if (this.type === "character") {
-      this.system.lifePoints.max =
-        Object.keys(this.system.abilities).map(i => this.system.abilities[i]).reduce((acc, a, i) => acc += Number(a), 0)
-        + Object.keys(this.system.skills).map(i => this.system.skills[i]).reduce((acc, s, i) => acc += Number(s.level), 0);
-
-      this.system.prayerPoints.max = this.system.skills.prayer.level * 3;
-      if (this.system.prayerPoints.value > this.system.prayerPoints.max) {
-        this.system.prayerPoints.value = this.system.prayerPoints.max;
-      }
-    }
-    if (this.system.lifePoints.value > this.system.lifePoints.max) {
-      this.system.lifePoints.value = this.system.lifePoints.max;
-    }
+    this._prepareCharacterBaseData(this);
+    this.system.lifePoints.value = RSKMath.clamp_value(this.system.lifePoints.value, this.system.lifePoints);
   }
 
   /**
@@ -60,10 +48,9 @@ export default class RSKActor extends Actor {
     const damageAfterSoak = this._applyArmourSoak(amount);
     const damageAfterSoakAndModifiers = this._applyIncomingDamageModifiers(damageAfterSoak);
     let remainingLifePoints = { ...this.system.lifePoints };
-    remainingLifePoints.value = this.system.lifePoints.value - damageAfterSoakAndModifiers
-    remainingLifePoints.value = remainingLifePoints.value < 0
-      ? 0
-      : remainingLifePoints.value;
+    remainingLifePoints.value = RSKMath.clamp_value(
+      this.system.lifePoints.value - damageAfterSoakAndModifiers,
+      { min: 0 });
     this.update({ system: { lifePoints: remainingLifePoints } });
   }
 
@@ -74,7 +61,7 @@ export default class RSKActor extends Actor {
 
   _applyArmourSoak(damage) {
     let armourValue = this._getArmourSoakValue();
-    return armourValue >= damage ? 0 : damage - armourValue;
+    return RSKMath.clamp_value(damage - armourValue, { min: 0 });
   }
 
   // todo: these two methods for calculating armour soak may be good to put in 
@@ -100,5 +87,21 @@ export default class RSKActor extends Actor {
     if (actorData.type !== 'character') return;
 
     const systemData = actorData.system;
+  }
+
+  _prepareCharacterBaseData(actorData) {
+    if (actorData.type !== 'character') return;
+
+    const systemData = actorData.system;
+    if (this.type === "character") {
+      systemData.lifePoints.max =
+        Object.keys(systemData.abilities).map(i => systemData.abilities[i]).reduce((acc, a, i) => acc += Number(a), 0)
+        + Object.keys(systemData.skills).map(i => systemData.skills[i]).reduce((acc, s, i) => acc += Number(s.level), 0);
+
+      systemData.prayerPoints.max = systemData.skills.prayer.level * 3;
+      systemData.prayerPoints.value = RSKMath.clamp_value(systemData.prayerPoints.value, systemData.prayerPoints)
+      systemData.summoningPoints.max = systemData.skills.summoning.level * 5;
+      systemData.summoningPoints.value = RSKMath.clamp_value(systemData.summoningPoints.value, systemData.summoningPoints)
+    }
   }
 }
---
+++ module/rsk-math.js
@@ -0,0 +1,7 @@
+class RSKMath {
+    static clamp_value(val, opts) {
+        if (val > opts.max ?? val) return max;
+        if (val < opts.min ?? val) return min;
+        return val;
+    }
+}
