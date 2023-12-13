3cca36f16884b6539654185efe38d4b6aa48b065
Quinten Marsala <quinten.marsala@gmail.com>
2023-12-12 16:13:51 -0800

    determine suppression logic

--- module/documents/RSKActiveEffect.js
+++ module/documents/RSKActiveEffect.js
@@ -1,5 +1,19 @@
 export default class RSKActiveEffect extends ActiveEffect {
+    isSuppressed = false;
+
     prepareData() {
         super.prepareData();
+    }
+
+    determineSuppression() {
+        this.isSuppressed = false;
+        if (this.disabled || (this.parent.documentName !== "Actor")) return;
+        const parts = this.origin?.split(".") ?? [];
+        const [parentType, parentId, documentType, documentId, syntheticItem, syntheticItemId] = parts;
+        let item;
+        if ((parentId !== this.parent.id) || (documentType !== "Item")) return;
+        item = this.parent.items.get(documentId);
+        if (!item) return;
+        this.isSuppressed = !item.isEquipped;
     }
 }
