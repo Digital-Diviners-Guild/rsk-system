d793765d11be540ea98b905e9292cbdb39ebecd4
Quinten Marsala <quinten.marsala@gmail.com>
2023-12-15 13:22:46 -0800

    add some more statuses

--- module/effects/statuses.js
+++ module/effects/statuses.js
@@ -5,9 +5,10 @@ const changeModes = {
 
 export function customizeStatusEffects() {
     CONFIG.statusEffects = [...rskEffects];
+    console.log(CONFIG.statusEffects);
 };
 
-//todo: need image urls
+//todo: need image urls (most are just made up)
 //todo: need other status we want on the token hud
 export const rskEffects = [
     {
@@ -21,5 +22,87 @@ export const rskEffects = [
                 value: 0
             }
         ]
+    },
+    {
+        id: "poisoned",
+        label: "RSK.CustomStatus.poisoned",
+        icon: "icons/svg/poisoned.svg",
+        changes: []
+    },
+    {
+        id: "bleeding",
+        label: "RSK.CustomStatus.bleeding",
+        icon: "icons/svg/bleeding.svg",
+        changes: []
+    },
+    {
+        id: "frozen",
+        label: "RSK.CustomStatus.frozen",
+        icon: "icons/svg/frozen.svg",
+        changes: []
+    },
+    {
+        id: "burning",
+        label: "RSK.CustomStatus.burning",
+        icon: "icons/svg/burning.svg",
+        changes: []
+    },
+    {
+        id: "prone",
+        label: "RSK.CustomStatus.prone",
+        icon: "icons/svg/prone.svg",
+        changes: []
+    },
+    {
+        id: "pinned",
+        label: "RSK.CustomStatus.pinned",
+        icon: "icons/svg/pinned.svg",
+        changes: []
+    },
+    {
+        id: "stunned",
+        label: "RSK.CustomStatus.stunned",
+        icon: "icons/svg/stunned.svg",
+        changes: []
+    },
+    // do we want to use statuses for magical/prayer buff/debuff/effects?
+    // if we do not use statuses for magic/prayer, how else will we track/show it? this may be a good fit.
+    // also some of these things like cursed and vulnerable are the same effect to a stronger degree.
+    // does that change anything?
+    {
+        id: "confused",
+        label: "RSK.CustomStatus.confused",
+        icon: "icons/svg/confused.svg",
+        changes: []
+    },
+    {
+        id: "cursed",
+        label: "RSK.CustomStatus.cursed",
+        icon: "icons/svg/cursed.svg",
+        changes: []
+    },
+    {
+        id: "enfeebled",
+        label: "RSK.CustomStatus.enfeebled",
+        icon: "icons/svg/enfeebled.svg",
+        changes: []
+    },
+    {
+        id: "vulnerable",
+        label: "RSK.CustomStatus.vulnerable",
+        icon: "icons/svg/vulnerable.svg",
+        changes: []
+    },
+    {
+        id: "weakened",
+        label: "RSK.CustomSTatus.weakened",
+        icon: "icons/svg/weakened.svg",
+        changes: []
+    },
+    {
+        id: "claws_of_guthix",
+        label: "RSK.CustomSTatus.claws_of_guthix",
+        icon: "icons/svg/claws_of_guthix.svg",
+        changes: []
     }
 ];
