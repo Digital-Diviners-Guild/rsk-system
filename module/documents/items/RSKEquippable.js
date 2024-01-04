658af92a28bc82b6c34cfa0411cbbb34051e7ed6
Quinten Marsala <quinten.marsala@gmail.com>
2024-01-04 13:30:12 -0800

    qualities can be added to more than just equippables

--- module/documents/items/RSKEquippable.js
+++ module/documents/items/RSKEquippable.js
@@ -1,4 +1,6 @@
-export default class RSKEquippable extends Item {
+import RSKItem from "./RSKItem.js";
+
+export default class RSKEquippable extends RSKItem {
     get isEquipped() {
         return this.system.equipped && this.system.equipped.isEquipped;
     }
@@ -11,22 +13,5 @@ export default class RSKEquippable extends Item {
         if (!this.system.equipped) return;
         this.system.equipped.isEquipped = !this.system.equipped.isEquipped;
         this.update({ "system.equipped": { ...this.system.equipped } });
-    }
-
-    addQuality(newQuality) {
-        if (newQuality.type !== "quality") return;
-        if (this.hasQuality(newQuality.sourceUuid)) return;
-
-        const updatedQualities = [...this.system.qualities ?? [], newQuality];
-        this.update({ "system.qualities": updatedQualities });
-    }
-
-    removeQuality(qualitySourceUuid) {
-        const updatedQualities = this.system.qualities?.filter(x => x.sourceUuid !== qualitySourceUuid);
-        this.update({ "system.qualities": updatedQualities });
-    }
-
-    hasQuality(uuid) {
-        return this.system.qualities?.filter(q => q.sourceUuid === uuid)?.length > 0 ?? false;
     }
 }
--- module/documents/items/RSKItem.js
+++ module/documents/items/RSKItem.js
@@ -1,3 +1,18 @@
 export default class RSKItem extends Item {
+    addQuality(newQuality) {
+        if (newQuality.type !== "quality") return;
+        if (this.hasQuality(newQuality.sourceUuid)) return;
 
+        const updatedQualities = [...this.system.qualities ?? [], newQuality];
+        this.update({ "system.qualities": updatedQualities });
+    }
+
+    removeQuality(qualitySourceUuid) {
+        const updatedQualities = this.system.qualities?.filter(x => x.sourceUuid !== qualitySourceUuid);
+        this.update({ "system.qualities": updatedQualities });
+    }
+
+    hasQuality(uuid) {
+        return this.system.qualities?.filter(q => q.sourceUuid === uuid)?.length > 0 ?? false;
+    }
 }
