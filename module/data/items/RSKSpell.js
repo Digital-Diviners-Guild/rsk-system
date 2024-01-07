8baf07c05a64e9381534f096249006236c86f263
Quinten Marsala <quinten.marsala@gmail.com>
2024-01-07 09:16:10 -0800

    remove comment

--- module/data/items/RSKSpell.js
+++ module/data/items/RSKSpell.js
@@ -1,11 +1,6 @@
 import RSKAction from "./RSKAction.js";
 import { fields } from "./fields.js";
 
-//is this and rskprayer a sub type of rskaction?
-// or are these more so spelldata and prayerdata that
-// rskaction would use to create the 'cast,pray,summon' actions?
-// we probably just need those three actions to cover all of magic, prayer, and summoning.
-// then an action for melee and another for range
 export default class RSKSpell extends RSKAction {
     static defineSchema() {
         return {
