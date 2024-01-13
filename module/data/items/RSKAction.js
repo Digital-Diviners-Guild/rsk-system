5ede63853303ec15dd3b934415a678796bccdefe
Quinten Marsala <quinten.marsala@gmail.com>
2024-01-13 13:31:41 -0800

    simplify action type for mvp

--- module/data/items/RSKAction.js
+++ module/data/items/RSKAction.js
@@ -1,6 +1,5 @@
 import { fields } from "./fields.js";
 
-//todo: trim model for manual
 export default class RSKAction extends foundry.abstract.DataModel {
     static defineSchema() {
         return {
@@ -10,20 +9,13 @@ export default class RSKAction extends foundry.abstract.DataModel {
             description: new fields.HTMLField(), // what it look like
             effectDescription: new fields.HTMLField(), // what it does
             damageEntries: new fields.ObjectField(),
-            requiredEquipment: new fields.ArrayField(new fields.ObjectField()),
+            requiredEquipment: new fields.StringField(),
             usageCost: new fields.ArrayField(new fields.SchemaField({
                 itemType: new fields.StringField(),// rune / ammo / points
                 type: new fields.StringField(), // air / arrow / prayer
                 amount: new fields.NumberField()
             })),
-            targets: new fields.ArrayField(new fields.SchemaField({
-                range: new fields.StringField(),
-                type: new fields.StringField(),
-                scope: new fields.StringField(),
-                number: new fields.NumberField()
-            })),
             statuses: new fields.ArrayField(new fields.StringField()),
-            effects: new fields.ArrayField(new fields.ObjectField()),
             //todo: I think we can reference the quality type here
             qualities: new fields.ArrayField(new fields.ObjectField()),
         };
