e21f348ae7208a5bedc38721624ecb7e02333d28
Quinten Marsala <quinten.marsala@gmail.com>
2024-01-14 22:27:30 -0800

    remove test item

--- module/documents/actors/RSKNpc.js
+++ module/documents/actors/RSKNpc.js
@@ -1,16 +1,6 @@
-import RSKAction from "../../data/items/RSKAction.js";
 import RSKActor from "./RSKActor.js";
 
 export default class RSKNpc extends RSKActor {
-    testActions = [];
-
-    prepareData() {
-        super.prepareData();
-        this.testActions = [];
-        this.testActions.push(RSKAction.fromSource({ id: "testAction", label: "test action", damageEntries: { stab: 3 } }));
-        console.log(this.testActions);
-    }
-
     getArmourValue() {
         return this.system.armourValue;
     }
