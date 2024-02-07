c03806607e02d296ef6fbce45c48ad2cce7605d2
Quinten Marsala <quinten.marsala@gmail.com>
2024-02-07 15:16:15 -0800

    spell fields

--- module/data/items/RSKSpell.js
+++ module/data/items/RSKSpell.js
@@ -7,15 +7,27 @@ export default class RSKSpell extends foundry.abstract.TypeDataModel {
             // what should this block be referred to as? it is common across things that can be used with an action
             description: new fields.StringField(),
             effectDescription: new fields.StringField(),
-            damageEntries: new fields.ObjectField(),
-            range: new fields.StringField({ initial: "far", options: [...Object.keys(CONFIG.RSK.ranges)] }),
+            damageEntries: new fields.SchemaField(Object.keys(CONFIG.RSK.damageTypes)
+                .reduce((obj, damageType) => {
+                    obj[damageType] = new fields.NumberField({ ...positiveNumberField, max: 500 });
+                    return obj;
+                }, {})),
+            range: new fields.StringField({
+                required: true,
+                initial: "far",
+                options: [...Object.keys(CONFIG.RSK.ranges)]
+            }),
             //todo: what is the diff between choices and options?
             usageCost: new fields.ArrayField(new fields.SchemaField({
                 type: new fields.StringField({ choices: [...Object.keys(CONFIG.RSK.runeType)] }),
                 amount: new fields.NumberField()
             })),
             usageCostLabel: new fields.StringField(),
-            spellType: new fields.StringField({ initial: "utility", options: [...Object.keys(CONFIG.RSK.spellTypes)] }),
+            spellType: new fields.StringField({
+                required: true,
+                initial: "utility",
+                options: [...Object.keys(CONFIG.RSK.spellTypes)]
+            }),
             statuses: new fields.ArrayField(new fields.StringField()),
             qualities: new fields.StringField(),
             requiredEquipment: new fields.StringField(),
