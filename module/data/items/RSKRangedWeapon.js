1782941c57d1a10b1d749909e971e259b83e440d
Quinten Marsala <quinten.marsala@gmail.com>
2024-02-07 15:15:42 -0800

    ranged fields

--- module/data/items/RSKRangedWeapon.js
+++ module/data/items/RSKRangedWeapon.js
@@ -6,22 +6,43 @@ import RSKEquippableType from "./RSKEquippableType.js";
 export default class RSKRangedWeapon extends RSKEquippableType {
     static defineSchema() {
         return {
-            type: new fields.StringField({ initial: "simple", options: [Object.keys(CONFIG.RSK.weaponTypes)] }),
-            material: new fields.StringField({ initial: "bronze", options: [Object.keys(CONFIG.RSK.weaponMaterials)] }),
-            ammoType: new fields.StringField({ initial: "arrow", options: [Object.keys(CONFIG.RSK.ammunitionType)] }),
+            type: new fields.StringField({
+                required: true,
+                initial: "simple",
+                options: [Object.keys(CONFIG.RSK.weaponTypes)]
+            }),
+            material: new fields.StringField({
+                required: true,
+                initial: "bronze",
+                options: [Object.keys(CONFIG.RSK.weaponMaterials)]
+            }),
+            ammoType: new fields.StringField({
+                required: true,
+                initial: "arrow",
+                options: [Object.keys(CONFIG.RSK.ammunitionType)]
+            }),
             description: new fields.StringField(),
-            range: new fields.StringField({ initial: "far", options: [...Object.keys(CONFIG.RSK.ranges)] }),
+            range: new fields.StringField({
+                required: true,
+                initial: "far",
+                options: [...Object.keys(CONFIG.RSK.ranges)]
+            }),
             uses: new fields.StringField(),
             effectDescription: new fields.StringField(),
             cost: new fields.NumberField({ ...costField }),
-            damageEntries: new fields.SchemaField(Object.keys(CONFIG.RSK.damageTypes).reduce((obj, damageType) => {
-                obj[damageType] = new fields.NumberField({ ...positiveNumberField, max: 150 });
-                return obj;
-            }, {})),
+            damageEntries: new fields.SchemaField(Object.keys(CONFIG.RSK.damageTypes)
+                .reduce((obj, damageType) => {
+                    obj[damageType] = new fields.NumberField({ ...positiveNumberField, max: 500 });
+                    return obj;
+                }, {})),
 
             // todo: this is all very much still experimental.  
             // how do we want to handle 'is equipable, and is stowable'
-            activeSlot: new fields.StringField({ initial: "weapon", options: ["weapon", "arm"] }),
+            activeSlot: new fields.StringField({
+                required: true,
+                initial: "weapon",
+                options: ["weapon", "arm"]
+            }),
             isEquipped: new fields.BooleanField({ initial: false }),
             maxStackSize: new fields.NumberField({ initial: 1 }),
             quantity: new fields.NumberField({ initial: 1 })
