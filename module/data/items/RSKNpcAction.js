9fd017a9340e1000cf49de8cdc20148224fde4ff
Quinten Marsala <quinten.marsala@gmail.com>
2024-02-07 15:14:20 -0800

    npc action fields

--- module/data/items/RSKNpcAction.js
+++ module/data/items/RSKNpcAction.js
@@ -2,12 +2,19 @@ import { fields } from "../fields.js";
 export default class RSKNpcAction extends foundry.abstract.TypeDataModel {
     static defineSchema() {
         return {
-            type: new fields.StringField({ initial: "melee", options: [...Object.keys(CONFIG.RSK.actionTypes)] }),
-            label: new fields.StringField(),
+            type: new fields.StringField({
+                required: true,
+                initial: "melee",
+                options: [...Object.keys(CONFIG.RSK.actionTypes)]
+            }),
             description: new fields.StringField(),
             effectDescription: new fields.StringField(),
-            damageEntries: new fields.ObjectField(),
-            range: new fields.StringField({ initial: "near", options: [...Object.keys(CONFIG.RSK.ranges)] }),
+            damageEntries: new fields.SchemaField(Object.keys(CONFIG.RSK.damageTypes)
+                .reduce((obj, damageType) => {
+                    obj[damageType] = new fields.NumberField({ ...positiveNumberField, max: 500 });
+                    return obj;
+                }, {})),
+            range: new fields.StringField({ required: true, initial: "near", options: [...Object.keys(CONFIG.RSK.ranges)] }),
         };
     }
 }
