808ec62edcad4c119c9edbef61bc54009add3c64
Quinten Marsala <quinten.marsala@gmail.com>
2024-02-08 12:01:06 -0800

    start making weapon template

--- module/data/items/RSKWeapon.js
+++ module/data/items/RSKWeapon.js
@@ -4,18 +4,23 @@ import RSKEquippableType from "./RSKEquippableType.js";
 export default class RSKWeapon extends RSKEquippableType {
     static defineSchema() {
         return {
-            attackTypes: new fields.ArrayField(new fields.StringField()),//attackType: ranged melee thrown ammo
+            //todo: config values for localization like others
+            attackTypes: new fields.ArrayField(new fields.StringField(
+                // not sure if magic even applies here.  
+                // a staff is actually a melee weapon if you don't cast a spell
+                // ammo and thrown still need to resolve to 'ranged' during the usage. (which is actionType encapsulated by action logic)
+                { initial: "melee", options: ["melee", "ranged", "magic", "thrown", "ammo"] }
+            )),
             weaponType: new fields.StringField({
                 required: true,
                 initial: "simple",
                 options: [...Object.keys(CONFIG.RSK.weaponTypes)]
             }),
-            // todo: materials
             material: new fields.StringField({
                 required: true,
                 initial: "bronze",
                 options: [
-                    ...Object.keys(CONFIG.RSK.ammunitionMaterialType)]
+                    ...Object.keys(CONFIG.RSK.weaponMaterials)]
             }),
             ammoType: new fields.StringField({
                 initial: "",
@@ -33,7 +38,7 @@ export default class RSKWeapon extends RSKEquippableType {
                     obj[damageType] = new fields.NumberField({ ...positiveNumberField, max: 500 });
                     return obj;
                 }, {})),
-            // todo: array of objects really - also need to rename qualities to specialEffects
+            //todo: array of objects eventually
             specialEffects: new fields.StringField(),
             // todo: ammo slot
             activeSlot: new fields.StringField({
