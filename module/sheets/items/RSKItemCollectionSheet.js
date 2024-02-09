57f1ca1e34a3b0011d10493da28aa1b469c49bbc
Quinten Marsala <quinten.marsala@gmail.com>
2024-02-09 09:04:57 -0800

    unneeded import and comment

--- module/sheets/items/RSKItemCollectionSheet.js
+++ module/sheets/items/RSKItemCollectionSheet.js
@@ -1,12 +1,6 @@
 import RSKItemSheet from "./RSKItemSheet.js";
-import { calculateUsedSlots, canAddItem } from "../../rsk-inventory.js";
+import { canAddItem } from "../../rsk-inventory.js";
 
-//this is very similar to inventory. I wonder if a 'container' abstraction is hiding somewhere
-// in this and the inventory
-//currently this allows you to cheese the stack limit of 3 on non ammo
-// but fixing it doesn't seem worth it, better to rework the inventory rules
-// into something that can be reused here. 
-//todo: refac to use inventory logic
 export default class RSKItemCollectionSheet extends RSKItemSheet {
     invalidTypes = ["action", "spell", "prayer", "summoning"];
 
