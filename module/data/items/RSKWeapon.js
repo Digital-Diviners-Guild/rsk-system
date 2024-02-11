f2ea7b2c3e7edf193570c1ef591c4bf0430d31ec
Quinten Marsala <quinten.marsala@gmail.com>
2024-02-10 21:44:49 -0800

    cleanup validator

--- module/data/items/RSKWeapon.js
+++ module/data/items/RSKWeapon.js
@@ -1,4 +1,4 @@
-import { costField, fields, positiveNumberField, AttackMethodField } from "../fields.js";
+import { costField, fields, positiveNumberField } from "../fields.js";
 import RSKEquippableType from "./RSKEquippableType.js";
 
 export default class RSKWeapon extends RSKEquippableType {
