33e518d3b703dff64b9c5994fb79c42d63ef8588
Quinten Marsala <quinten.marsala@gmail.com>
2024-03-17 13:38:46 -0700

    move npc action to npc action

--- module/data/items/RSKNpcAction.js
+++ module/data/items/RSKNpcAction.js
@@ -1,6 +1,6 @@
-import { fields, positiveNumberField } from "../fields.js";
+import { fields } from "../fields.js";
 export default class RSKNpcAction extends foundry.abstract.TypeDataModel {
-    //todo: could this utilize the 'outcomes' too?
+    //todo: need to update sheet to use outcomes
     static defineSchema() {
         return {
             type: new fields.StringField({
@@ -10,12 +10,29 @@ export default class RSKNpcAction extends foundry.abstract.TypeDataModel {
             }),
             description: new fields.StringField(),
             effectDescription: new fields.StringField(),
-            damageEntries: new fields.SchemaField(Object.keys(CONFIG.RSK.damageTypes)
-                .reduce((obj, damageType) => {
-                    obj[damageType] = new fields.NumberField({ ...positiveNumberField, max: 500 });
-                    return obj;
-                }, {})),
+            targetOutcomes: new fields.ArrayField(new fields.ObjectField()),
+            usageOutcomes: new fields.ArrayField(new fields.ObjectField()),
             range: new fields.StringField({ required: true, initial: "near", choices: [...Object.keys(CONFIG.RSK.ranges)] }),
         };
+    }
+
+    async use(actor) {
+        const content = await renderTemplate("systems/rsk/templates/applications/action-message.hbs",
+            {
+                name: this.parent.name,
+                ...this,
+                hideRollResults: true
+            });
+
+        await ChatMessage.create({
+            content: content,
+            flags: {
+                rsk: {
+                    actionType: this.type,
+                    ...this,
+                    outcomes: [...this.targetOutcomes]
+                }
+            }
+        });
     }
 }
