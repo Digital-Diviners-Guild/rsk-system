b493584ed7d6ccdbf760afb93532ed7afd71dbe2
Quinten Marsala <quinten.marsala@gmail.com>
2024-03-25 16:09:18 -0700

    fix codex sheet

--- module/sheets/items/RSKCodexSheet.js
+++ module/sheets/items/RSKCodexSheet.js
@@ -1,7 +1,27 @@
 export default class RSKCodexSheet extends ItemSheet {
+    static get defaultOptions() {
+        return mergeObject(super.defaultOptions, {
+            classes: ["rsk", "sheet", "item"],
+            width: 600,
+            height: 600,
+            tabs: [{ navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "description" }],
+            dragDrop: [{ dropSelector: "[data-can-drop=true]" }],
+        });
+    }
+
     get template() {
         return 'systems/rsk/templates/items/codex-sheet.hbs';
     }
+
+    getData() {
+        const context = super.getData();
+        const itemData = context.item;
+        context.system = itemData.system;
+        context.flags = itemData.flags;
+        context.config = CONFIG.RSK;
+        return context;
+    }
+
 
     activateListeners(html) {
         super.activateListeners(html);
@@ -26,7 +46,8 @@ export default class RSKCodexSheet extends ItemSheet {
 
     async _onDropItem(event, data) {
         const item = await Item.fromDropData(data);
-        if (item.type !== this.item.system.actionType
+        debugger;
+        if (item.system.category !== this.item.system.actionType
             || this.item.system.actions.filter(a => a.itemId === item._id).length > 0) return;
 
         this.item.update({ "system.actions": [...this.item.system.actions, { itemId: item._id, name: item.name }] })
