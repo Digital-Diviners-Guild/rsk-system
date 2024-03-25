dea90d8dfc1ee7a5d6141a44ce382b4296bc4010
Quinten Marsala <quinten.marsala@gmail.com>
2024-03-24 17:47:16 -0700

    support spells

--- module/data/items/RSKCastable.js
+++ module/data/items/RSKCastable.js
@@ -1,8 +1,7 @@
-import { uiService } from "../../rsk-ui-service.js";
 import RSKItemType from "./RSKItemType.js";
 
 export default class RSKCastable extends RSKItemType {
-    async canUse(actor) {
+    canUse(actor) {
         const canUseHandlers = {
             spell: () => this.usageCost.every(uc =>
                 actor.items.find(r => r.system.category === "rune"
@@ -16,32 +15,6 @@ export default class RSKCastable extends RSKItemType {
         return canUseHandlers[this.category]();
     }
 
-    async use(actor) {
-        if (!this.canUse(actor)) return;
-
-        const rollData = this._prepareRollData(actor);
-        const confirmRollResult = await uiService.showDialog("confirm-roll", rollData);
-        if (!confirmRollResult.confirmed) return;
-
-        const skillResult = await actor.system.useSkill(confirmRollResult);
-        const actionOutcome = this._prepareOutcomeData();
-        const flavor = await renderTemplate("systems/rsk/templates/applications/action-message.hbs",
-            {
-                ...skillResult,
-                ...actionOutcome
-            });
-        await skillResult.toMessage({
-            flavor: flavor,
-            flags: {
-                rsk: {
-                    ...skillResult,
-                    ...actionOutcome
-                }
-            }
-        });
-        this._handleUsageCost(actor);
-    }
-
     _prepareRollData(actor) {
         return {
             ...actor.system.getRollData(),
@@ -52,18 +25,20 @@ export default class RSKCastable extends RSKItemType {
         };
     }
 
-    _prepareOutcomeData() {
+    _prepareOutcomeData(actor) {
         return {
+            actorUuid: actor.uuid,
             name: this.parent.name,
             description: this.effectDescription,
             actionType: this.category,
             img: this.parent.img,
-            outcome: { ...this.targetOutcomes },
+            targetOutcome: { ...this.targetOutcome },
+            actorOutcome: { ...this.usageOutcome },
             specialEffect: [...this.specialEffect]
         };
     }
 
-    _handleUsageCost(actor) {
+    _handleItemUsed(actor, skillResult) {
         const costHandlers = {
             prayer: (success) => success
                 ? actor.system.spendPoints("prayerPoints", this.usageCost[0]?.amount ?? 0)
@@ -71,10 +46,10 @@ export default class RSKCastable extends RSKItemType {
             summoning: (success) => success
                 ? actor.system.spendPoints("summoningPoints", this.usageCost[0]?.amount ?? 0)
                 : actor.system.spendPoints("summoningPoints", 1),
-            magic: (success) => success
+            spell: (success) => success
                 ? this.usageCost.forEach(uc => actor.system.spendRunes(uc.type, uc.amount))
                 : []
         }
-        costHandlers[this.category]();
+        costHandlers[this.category](skillResult.isSuccess);
     }
 }
